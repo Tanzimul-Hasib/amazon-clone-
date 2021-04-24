b0a2165036b74d3c84e65576a50fc04a02c4596f
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2021-04-25 01:11:01 +0600

    Environment is ready to create new functionality

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxPyrPEAGvo1syxJcCAK9I60U8JfEa9DE",
+  authDomain: "hasib-2.firebaseapp.com",
+  projectId: "hasib-2",
+  storageBucket: "hasib-2.appspot.com",
+  messagingSenderId: "234748695242",
+  appId: "1:234748695242:web:025b5c98bb69ca20c75a33",
+  measurementId: "G-L2LE7GWHVL",
+};
