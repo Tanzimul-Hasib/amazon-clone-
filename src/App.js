ac4815fa85c55657d18467b6dbbb02cd6e8b1008
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2021-04-25 16:04:30 +0600

    Home page product component design complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      {/* Home */}
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product
+            title="The lean Startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_1x._SY304_CB431800965_.jpg"
+            rating={}
+          />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
