447d7c3e07bdddb17272a5c17243f0b0105090a8
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2021-04-27 17:48:28 +0600

    checkout page design complete with remove from basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Subtotal from "./Subtotal";
 import "./Checkout.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
         <div>
           <h2 className="checkout__title">Shopping Cart</h2>
-          {/* basket items */}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
--- src/Home.js
+++ src/Home.js
@@ -13,15 +13,28 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id="1"
             title="The lean Startup"
             price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/31jBba7+ySL.jpg"
+            rating={5}
+          />
+          <Product
+            id="2"
+            title="AmazonBasics Premium 48Gb"
+            price={13}
+            image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_520x520._SY304_CB442725065_.jpg"
+            rating={5}
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="3"
+            title="Computers & Accessories"
+            price={13}
             image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_PC_1x._SY304_CB431800965_.jpg"
             rating={5}
           />
-          <Product />
-        </div>
-        <div className="home__row">
-          <Product />
           <Product />
           <Product />
         </div>
